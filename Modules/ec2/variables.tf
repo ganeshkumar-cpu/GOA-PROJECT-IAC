3db523c021879d3c4435cc2cd89022b6b62794d9
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 14:20:01 +0530

    Update variables.tf

--- Modules/ec2/variables.tf
+++ Modules/ec2/variables.tf
@@ -1,4 +1,24 @@
-variable "vpc_id" {}
-variable "api_server_1_sg_id" {}
-variable "api_server_2_sg_id" {}
-variable "web_db_server_sg_id" {}
+# modules/ec2/variables.tf
+variable "api_server_1_ami" {
+  description = "AMI ID for the first API server"
+}
+
+variable "api_server_2_ami" {
+  description = "AMI ID for the second API server"
+}
+
+variable "web_db_server_ami" {
+  description = "AMI ID for the Web DB server"
+}
+
+variable "api_server_1_sg_id" {
+  description = "The security group ID for the API Server 1"
+}
+
+variable "api_server_2_sg_id" {
+  description = "The security group ID for the API Server 2"
+}
+
+variable "web_db_server_sg_id" {
+  description = "The security group ID for the Web DB Server"
+}
