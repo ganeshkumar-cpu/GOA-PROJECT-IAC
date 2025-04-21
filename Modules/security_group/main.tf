93bcac878fec31186c33f9eecff6a23ebb8645e8
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 14:01:42 +0530

    Create main.tf

---
+++ Modules/security_group/main.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "api_server_1_sg" {
+  name        = "api_server_1_sg"
+  vpc_id      = var.vpc_id
+  description = "Allow traffic for API Server 1"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "api_server_2_sg" {
+  name        = "api_server_2_sg"
+  vpc_id      = var.vpc_id
+  description = "Allow traffic for API Server 2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "web_db_server_sg" {
+  name        = "web_db_server_sg"
+  vpc_id      = var.vpc_id
+  description = "Allow traffic for Web DB Server"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
