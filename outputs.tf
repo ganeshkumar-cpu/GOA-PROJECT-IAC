0f25a8ee47559bc33aea106f6c55cf8fefa622fb
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 15:09:53 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,14 @@ output "api_server_2_id" {
 output "web_db_server_id" {
   value = module.ec2_instances.web_db_server_id
 }
+output "public_subnet_id" {
+  value = module.vpc.public_subnet_id
+}
+
+output "private_subnet_id" {
+  value = module.vpc.private_subnet_id
+}
+
+output "public_db_subnet_id" {
+  value = module.vpc.public_db_subnet_id
+}
