47241e1b47cb728a81caf4665e637f0ad411d845
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 15:00:51 +0530

    Create variables.tf

---
+++ Modules/VPC/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+
+variable "public_subnet_cidr" {}
+variable "public_subnet_az" {}
+
+variable "private_subnet_cidr" {}
+variable "private_subnet_az" {}
+
+variable "public_db_subnet_cidr" {}
+variable "public_db_subnet_az" {}
