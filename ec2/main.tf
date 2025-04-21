c03331afd596654f5eeff7daf35bc9b1a3feb2da
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 11:54:51 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,1 +1,34 @@
+provider "aws" {
+  region = "ap-south-1" 
+}
+
+module "api_server_2" {
+  source              = "./modules/ec2_instance"
+  name                = "api_server_2"
+  instance_type       = "t3a.xlarge"
+  ami_id              = var.ami_id
+  subnet_id           = var.subnet_id
+  security_group_ids  = var.security_group_ids
+  key_name            = var.key_name
+}
+
+module "api_server_1" {
+  source              = "./modules/ec2_instance"
+  name                = "api_server_1"
+  instance_type       = "t3a.xlarge"
+  ami_id              = var.ami_id
+  subnet_id           = var.subnet_id
+  security_group_ids  = var.security_group_ids
+  key_name            = var.key_name
+}
+
+module "web_db_server" {
+  source              = "./modules/ec2_instance"
+  name                = "Web_DB_server"
+  instance_type       = "m6g.4xlarge"
+  ami_id              = var.ami_id
+  subnet_id           = var.subnet_id
+  security_group_ids  = var.security_group_ids
+  key_name            = var.key_name
+}
 
