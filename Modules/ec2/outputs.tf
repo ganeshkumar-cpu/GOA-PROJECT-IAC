01ffaa201f4f11f3ea10c3952bffb4a5aac117e9
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 14:05:11 +0530

    Create outputs.tf

---
+++ Modules/ec2/outputs.tf
@@ -0,0 +1,11 @@
+output "api_server_1_id" {
+  value = aws_instance.api_server_1.id
+}
+
+output "api_server_2_id" {
+  value = aws_instance.api_server_2.id
+}
+
+output "web_db_server_id" {
+  value = aws_instance.web_db_server.id
+}
