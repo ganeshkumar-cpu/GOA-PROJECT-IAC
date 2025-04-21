fa3356ab741915e130c7d90d07f519535670db45
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 14:29:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
+# root main.tf
 provider "aws" {
   region = "ap-south-1"
 }
 
 module "vpc" {
-  source       = "./modules/vpc"
-  vpc_id       = "vpc-00defc46ce1817d4f"
-  vpc_name     = "Goa_vpc"
+  source   = "./modules/vpc"
+  vpc_id   = "vpc-00defc46ce1817d4f"
+  vpc_name = "Goa_vpc"
 }
 
 module "security_group" {
@@ -14,10 +15,14 @@ module "security_group" {
 }
 
 module "ec2_instances" {
-  source = "./modules/ec2"
-  vpc_id = "vpc-00defc46ce1817d4f"
-  api_server_1_sg_id = module.security_group.api_server_1_sg_id
-  api_server_2_sg_id = module.security_group.api_server_2_sg_id
-  web_db_server_sg_id = module.security_group.web_db_server_sg_id
+  source                = "./modules/ec2"
+  vpc_id                = "vpc-00defc46ce1817d4f"
+  api_server_1_sg_id    = module.security_group.api_server_1_sg_id
+  api_server_2_sg_id    = module.security_group.api_server_2_sg_id
+  web_db_server_sg_id   = module.security_group.web_db_server_sg_id
+  api_server_1_ami      = "ami-0dee22c13ea7a9a67"
+  api_server_2_ami      = "ami-0dee22c13ea7a9a67"  
+  web_db_server_ami     = "ami-0c8eea98010057bd0"  
 }
+
 
