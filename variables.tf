61b188eebdaf537c0a1e33cddf7e112074b44c99
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 11:08:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,64 @@
 variable "api_server_1_sg_id" {
-  description = "The security group ID for the API Server 1."
+  description = "The security group ID to be associated with API Server 1 instance."
 }
 
 variable "api_server_2_sg_id" {
-  description = "The security group ID for the API Server 2."
+  description = "The security group ID to be associated with API Server 2 instance."
 }
 
 variable "web_db_server_sg_id" {
-  description = "The security group ID for the Web DB Server."
+  description = "The security group ID to be associated with the Web DB Server instance."
 }
-variable "vpc_cidr" {}
-variable "vpc_name" {}
 
-variable "public_subnet_cidr" {}
-variable "public_subnet_az" {}
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+}
 
-variable "private_subnet_cidr" {}
-variable "private_subnet_az" {}
+variable "vpc_name" {
+  description = "The name tag for the VPC."
+}
 
-variable "public_db_subnet_cidr" {}
-variable "public_db_subnet_az" {}
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet (for API servers)."
+}
 
-variable "api_server_1_ami" {}
-variable "api_server_2_ami" {}
-variable "web_db_server_ami" {}
+variable "public_subnet_az" {
+  description = "Availability Zone for the public subnet."
+}
 
-variable "api_servers_key_name" {}
-variable "web_db_key_name" {}
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet (for internal resources)."
+}
+
+variable "private_subnet_az" {
+  description = "Availability Zone for the private subnet."
+}
+
+variable "public_db_subnet_cidr" {
+  description = "CIDR block for the public database subnet (used by Web DB Server)."
+}
+
+variable "public_db_subnet_az" {
+  description = "Availability Zone for the public database subnet."
+}
+
+variable "api_server_1_ami" {
+  description = "AMI ID to be used for provisioning API Server 1 instance."
+}
+
+variable "api_server_2_ami" {
+  description = "AMI ID to be used for provisioning API Server 2 instance."
+}
+
+variable "web_db_server_ami" {
+  description = "AMI ID to be used for provisioning the Web DB Server instance."
+}
+
+variable "api_servers_key_name" {
+  description = "Key pair name to SSH into the API servers."
+}
+
+variable "web_db_key_name" {
+  description = "Key pair name to SSH into the Web DB Server."
+}
+
